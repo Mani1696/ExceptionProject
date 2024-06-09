02edbd9136edfe2179db05d5bbdd5ebfeef7b674
Mani1696 <manibharathi809@gmail.com>
2024-06-09 12:29:48 +0530

    vote age changed 18 to 20

--- Vote.java
+++ Vote.java
@@ -3,7 +3,7 @@ package Exception;
 public class Vote {
 	public static void main (String[]args) throws VoteNotEligible {
 		int a=16;
-		if(a>=18) {
+		if(a>=20) {
 			System.out.println("Eligble for vote");
 		}
 		else {
